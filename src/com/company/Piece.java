4d44112c8e1e68b33ce07eacfb85e2ce3aad5406
L-DML <dominguess.laura@gmail.com>
2019-01-22 15:15:22 +0100

    Ajout des getter et des setter

--- src/com/company/Piece.java
+++ src/com/company/Piece.java
@@ -2,13 +2,37 @@ package com.company;
 
 public class Piece {
 
-    int x;
-    int y ;
-    String id;
+    private int x;
+    private int y ;
+    private String id;
 
     public Piece(int x, int y, String id) {
         this.x = x;
         this.y = y;
+        this.id = id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
         this.id = id;
     }
 }
