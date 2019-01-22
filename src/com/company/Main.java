4877e9ba85199c85032509a87ae3a92f39ffdc81
L-DML <dominguess.laura@gmail.com>
2019-01-22 18:11:27 +0100

    Petit Netoyage de printemps

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,53 +7,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Plateau pieces = new Plateau();
+        Plateau plateau = new Plateau();
 
         /** On crée pTaille ArrayList **/
 
-      pieces.creerPlateau();
-      pieces.afficherPlateau();
+      plateau.creerPlateau();
+      plateau.afficherPlateau();
 
 
 
-        // List<ArrayList<Piece>> pieces = new ArrayList<ArrayList<Piece>>(); *
 
-        /*Plateau plateau = new Plateau(4);
-
-        Piece p1 = new Piece (0,0, "A");
-        Piece p2 = new Piece(0, 1, "B");
-        Piece p3 = new Piece(0,2, "C");
-        Piece p4 = new Piece (1, 0, "D");
-        Piece p5 = new Piece(1,1, "E");
-        Piece p6 = new Piece(1,2, "F");
-        Piece p7 = new Piece(2, 0, "G");
-        Piece p8 = new Piece(2,1, "H");
-        Piece p9 = new Piece(2,2, " ");*/
-
-       /* plateau.ajouter(p1);
-        plateau.ajouter(p2);
-        plateau.ajouter(p3);
-        plateau.ajouter(p4);
-        plateau.ajouter(p5);
-        plateau.ajouter(p6);
-        plateau.ajouter(p7);
-        plateau.ajouter(p8);
-        plateau.ajouter(p9);*/
-
-
-        /*pieces.add(new ArrayList<Piece>());
-
-        pieces.get(0).add(p1);
-        pieces.get(0).add(p2);
-        pieces.get(0).add(p3);
-        pieces.get(1).add(p4);
-        pieces.get(1).add(p5);
-        pieces.get(1).add(p6);
-        pieces.get(2).add(p7);
-        pieces.get(2).add(p8);
-        pieces.get(2).add(p9);
-
-        System.out.println(plateau);*/
 
 
 
--- src/com/company/Plateau.java
+++ src/com/company/Plateau.java
@@ -13,9 +13,9 @@ public class Plateau  {
 
     /** pieces : ArrayList de ArrayList */
 
+    ArrayList<ArrayList<String>> pieces = new ArrayList<ArrayList<String>>(pTaille);
 
     /** Methode Création Plateau **/
-    ArrayList<ArrayList<String>> pieces = new ArrayList<ArrayList<String>>(pTaille);
 
     public void creerPlateau(){
 
@@ -41,6 +41,8 @@ public class Plateau  {
 
     }
 
+    /** Méthode Afficher le plateau **/
+
     public void afficherPlateau(){
 
         for (int i = 0; i < pieces.size(); i++) {
@@ -50,16 +52,7 @@ public class Plateau  {
             System.out.println();
         }
     }
-    // int pTaille = 3; // correspond à la longueur/largeur du carré
 
-    /** pieces : ArrayList de ArrayList */
-
-   //  ArrayList<ArrayList<Integer>>  pieces = new ArrayList<ArrayList<Integer>>(pTaille) ;
-
-    /** On crée pTaille ArrayList **/
-
-   /* ArrayList<Integer> l1 = new ArrayList<Integer>();
-    l1.add(0,0, "A");*/
 
 
     // List<ArrayList<Piece>> pieces ;
@@ -85,6 +78,46 @@ public class Plateau  {
         for( int i=0 ; i < pTaille-1 ; i++) {
             Piece piece = new Piece(i, )
         }*/
+
+
+    // List<ArrayList<Piece>> pieces = new ArrayList<ArrayList<Piece>>(); *
+
+        /*Plateau plateau = new Plateau(4);
+
+        Piece p1 = new Piece (0,0, "A");
+        Piece p2 = new Piece(0, 1, "B");
+        Piece p3 = new Piece(0,2, "C");
+        Piece p4 = new Piece (1, 0, "D");
+        Piece p5 = new Piece(1,1, "E");
+        Piece p6 = new Piece(1,2, "F");
+        Piece p7 = new Piece(2, 0, "G");
+        Piece p8 = new Piece(2,1, "H");
+        Piece p9 = new Piece(2,2, " ");*/
+
+       /* plateau.ajouter(p1);
+        plateau.ajouter(p2);
+        plateau.ajouter(p3);
+        plateau.ajouter(p4);
+        plateau.ajouter(p5);
+        plateau.ajouter(p6);
+        plateau.ajouter(p7);
+        plateau.ajouter(p8);
+        plateau.ajouter(p9);*/
+
+
+        /*pieces.add(new ArrayList<Piece>());
+
+        pieces.get(0).add(p1);
+        pieces.get(0).add(p2);
+        pieces.get(0).add(p3);
+        pieces.get(1).add(p4);
+        pieces.get(1).add(p5);
+        pieces.get(1).add(p6);
+        pieces.get(2).add(p7);
+        pieces.get(2).add(p8);
+        pieces.get(2).add(p9);
+
+        System.out.println(plateau);*/
 
 
 }
