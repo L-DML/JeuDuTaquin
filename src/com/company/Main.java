6508b4ac8fa0902925d3023b27a50a5394ff0a83
Pcattelin <pierre.cattelin@gmail.com>
2019-01-22 18:00:26 +0100

    Merge branch 'master' of C:\Users\pierr\IdeaProjects\JeuDuTaquin with conflicts.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,88 +1,46 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
+import static java.sql.DriverManager.println;
+import static jdk.nashorn.internal.objects.Global.print;
 
 public class Main {
 
     public static void main(String[] args) {
+        String[][] pieces;
+        String id;
+        int x;
+        int y;
 
-
-        int pTaille = 3; // correspond à la longueur/largeur du carré
-
-
-        /** pieces : ArrayList de ArrayList */
-
-        ArrayList<ArrayList<String>>  pieces = new ArrayList<ArrayList<String>>(pTaille) ;
-
-        /** On crée pTaille ArrayList **/
-
-        ArrayList<String> l0 = new ArrayList<String>();
-        l0.add("A");
-        l0.add("B");
-        l0.add("C");
-        pieces.add(l0);
-
-
-        ArrayList<String> l1 = new ArrayList<String>();
-        l1.add("D");
-        l1.add("E");
-        l1.add("F");
-        pieces.add(l1);
-
-        ArrayList<String> l2 = new ArrayList<String>();
-        l2.add("G");
-        l2.add("H");
-        l2.add(" ");
-        pieces.add(l2);
-
-        for (int i = 0; i < pieces.size(); i++) {
-            for (int j = 0; j < pieces.get(i).size(); j++) {
-                System.out.print(pieces.get(i).get(j) + " ");
+        public int[] coordPieceVide(){
+            int tab[];
+            for (int i = 0; i < pieces.length-1; i++) {
+                for (int j = 0; j < pieces.length-1; j++) {
+                    if (pieces[i][j] == " ") {
+                        println("Pièce vide trouvée !");
+                        tab[0] = i
+                        tab[1] = j;
+                    }
+                }
             }
-            System.out.println();
+            return tab;
         }
 
-        // List<ArrayList<Piece>> pieces = new ArrayList<ArrayList<Piece>>(); *
+        public void deplacer(int x, int y, String id) {
+            //On récupère l'emplacement de la piece vid
+            int[] pVide = pVide.coordPieceVide();
 
-        /*Plateau plateau = new Plateau(4);
+            /*Ca on s'en ballek pour l'instant d'abord faut que ça marche*/
+            /*int pX = x;
+            int pY = y;
+            String pID = id;*/
 
-        Piece p1 = new Piece (0,0, "A");
-        Piece p2 = new Piece(0, 1, "B");
-        Piece p3 = new Piece(0,2, "C");
-        Piece p4 = new Piece (1, 0, "D");
-        Piece p5 = new Piece(1,1, "E");
-        Piece p6 = new Piece(1,2, "F");
-        Piece p7 = new Piece(2, 0, "G");
-        Piece p8 = new Piece(2,1, "H");
-        Piece p9 = new Piece(2,2, " ");*/
+            /*On envoie le contenu ver*/
+            pieces[pVide[0], pVide[1]] = id;
+            println("La pièce : " + id + "a été déplacée.");
+        }
 
-       /* plateau.ajouter(p1);
-        plateau.ajouter(p2);
-        plateau.ajouter(p3);
-        plateau.ajouter(p4);
-        plateau.ajouter(p5);
-        plateau.ajouter(p6);
-        plateau.ajouter(p7);
-        plateau.ajouter(p8);
-        plateau.ajouter(p9);*/
-
-
-        /*pieces.add(new ArrayList<Piece>());
-
-        pieces.get(0).add(p1);
-        pieces.get(0).add(p2);
-        pieces.get(0).add(p3);
-        pieces.get(1).add(p4);
-        pieces.get(1).add(p5);
-        pieces.get(1).add(p6);
-        pieces.get(2).add(p7);
-        pieces.get(2).add(p8);
-        pieces.get(2).add(p9);
-
-        System.out.println(plateau);*/
-
-
-
+        String moveID = "A";
+//        deplacer(0,2,moveID);
+        System.out.println("Zeubi");
     }
 }
